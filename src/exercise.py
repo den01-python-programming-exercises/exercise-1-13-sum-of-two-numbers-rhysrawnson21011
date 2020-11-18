35765ce20cffdf4d2620ca4a95bb15bfbe20cd0e
rhysrawnson2101 <Rhysrawnson@gmail.com>
2020-11-18 00:43:58 +0000

    addition and subtraction

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #write your code below this line
+  first = int(input('Give the first number:'))
+  second = int(input('Give the second number:'))
+  print(first + second)
+
+
+  third = int(input('Give the third number:'))
+  fourth = int(input('give the third number:'))
+
+  print(third - fourth)
+
+
+
+
 
 if __name__ == '__main__':
     main()
